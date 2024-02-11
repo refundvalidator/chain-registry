a885aee88e5f3e6070088f13f6f2cd7c16bd0b06
refundvalidator <refundvalidator@gmail.com>
2024-02-11 12:49:34 -0600

    update workflow

---
+++ .github/workflows/utility/update_lists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+files="$(ls -d */ | grep -vE "^#|\.|_")"
+printf '%s\n' "{
+  \"mainnets\": [
+    \"${files//$'\n'/$'",\n    "'}\"
+  ]
+}" > mainnets.json
+files="$(ls -d testnets/*/ | grep -vE "^#|\.|_" | xargs -n 1 basename)"
+printf '%s\n' "{
+  \"testnets\": [
+    \"${files//$'\n'/$'",\n    "'}\"
+  ]
+}" > mainnets.json
